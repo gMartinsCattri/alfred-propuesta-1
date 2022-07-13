0b0de96f1b1eb18b2dfa9fdb049dbcb6ec165707
guimartinsalmeida <guimartinsalmeida@gmail.com>
2022-07-13 11:40:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
-import uberApp from './uber-app-removebg-preview.png'
-import cinepolisApp from './cinepolislogo-removebg-preview.png'
-import chedrauiApp from './chedraui-logo-removebg-preview.png'
+// import logo from './logo.svg';
+// import cinepolisApp from './cinepolislogo-removebg-preview.png'
+// import chedrauiApp from './chedraui-logo-removebg-preview.png'
 import cocaCola from './coca-cola-campaing.jpeg'
-import estacionamientoApp from './estacionamiento.png'
+// import estacionamientoApp from './estacionamiento.png'
 import appleApp from './apple-logo-removebg-preview.png'
 import americanEagleApp from './american-eagle-logo-removebg-preview.png'
+import aplazoLogo from './aplazo1.png'
 import disney from './Pottery-Barn-Kids-Successful-Banner-Ad.webp'
 import zara from './zara-campaing.jpeg'
+import adidasLogo from './adidas1.png'
+import liverpoolLogo from './liverpool1.png'
+import zaraLogo from './zara1.png'
+import pullandbearLogo from './Pull-Bear1.png'
+import underArmor from './Under-Armour1.png'
+import levis from './Levis1.png'
+import kipling from './kipling.png'
+import gap from './gap.png'
+import moyo from './moyo.png'
+import julio from './julio.png'
+import reservaTuMesa from './reserva-tu-mesa-alfred.png'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -57,14 +68,57 @@ function App() {
       <header className="App-header">
         
         <div className="container-app">
-        <div className="app-alfred"><img className="image-app-alfred" src={uberApp} alt="uber"></img></div>
-        <div className="app-alfred"><img className="image-app-alfred" src={cinepolisApp} alt="uber"></img></div>
-        <div className="app-alfred"><img className="image-app-alfred" src={chedrauiApp} alt="uber"></img></div>
-        <div className="app-alfred"><img className="image-app-alfred" src={estacionamientoApp} alt="uber"></img></div>
-        <div className="app-alfred"><img className="image-app-alfred" src={appleApp} alt="uber"></img></div>
-        <div className="app-alfred"><img className="image-app-alfred" src={americanEagleApp} alt="uber"></img></div>
+        <div className="app-alfred"></div>
+        <div className="app-alfred"></div>
+        <div className="app-alfred"></div>
+        <div className="app-alfred"></div>
+        <div className="app-alfred"></div>
+        <div className="app-alfred"></div>
         </div>
+        <div className="container-aplazo">         <button class="btn btn-primary aplazo-alfred" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop"><img className="aplazo-logo" src={aplazoLogo} alt="" /></button>
+        {/* <button class="btn btn-primary aplazo-alfred" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Estacionamiento</button> */}
+        <button type="button" class="btn btn-primary aplazo-alfred" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</button>
+</div>
 
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="offcanvas offcanvas-top dropdown-menu-aplazos" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+  
+  <div class="offcanvas-body">
+  <div className="container-app container-app-off-canvas-body">
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={adidasLogo} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={liverpoolLogo} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={zaraLogo} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={pullandbearLogo} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={underArmor} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={levis} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={kipling} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={gap} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={moyo} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={julio} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={appleApp} alt="uber"></img></div>
+        <div className="app-alfred canvas-app-alfred"><img className="image-app-alfred canvas-imagem-app-alfred" src={americanEagleApp} alt="uber"></img></div>
+        </div>  </div>
+
+       
+</div>
         <div className="slider-alfred">
         <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="false" data-bs-wrap="false" >
         <div className="carousel-indicators">
@@ -75,7 +129,7 @@ function App() {
         <div className="carousel-inner">
           <div className="carousel-item">
   
- <img src={logo} className="d-block w-100 slider-images" alt="..."></img>
+ <img src={reservaTuMesa} className="d-block w-100 slider-images" alt="..."></img>
   
           </div>
           <div className="carousel-item active">
